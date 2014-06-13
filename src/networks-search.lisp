4c9bd18d0bfb294339f51deb6f6fc5fd60b212b9
Kyle <kyle@tyrfingr.is>
2014-06-13 00:14:41 -0700

    Remove test network.

--- src/networks-search.lisp
+++ src/networks-search.lisp
@@ -14,22 +14,6 @@
 
 ;; "Lisp isn't a language, it's a building material. -Alan Kay"
 
-(defvar ARPA-NET
-  (list
-   (node 'sri  '(utah stan ucla ucsb))
-   (node 'ucsb '(sri  ucla))
-   (node 'ucla '(ucsb stan  sri rand))
-   (node 'stan '(sri  ucla))
-   (node 'utah '(sri  sdc  mit))
-   (node 'sdc  '(utah rand))
-   (node 'rand '(ucla bbn))
-   (node 'mit  '(utah bbn  linc))
-   (node 'bbn  '(rand mit  harv))
-   (node 'harv '(bbn  carn))
-   (node 'carn '(harv case))
-   (node 'case '(carn linc))
-   (node 'linc '(case mit))))
-
 (defun shorten-path (nodes path)
   (if (endp nodes)
       path
